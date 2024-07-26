3e8e4717182cc13136269518dfc60ce1d6e10bb9
Kayky Ribeiro <kaykykaro04@gmail.com>
2024-07-26 13:54:37 -0300

    tailwind

--- cbApp/App.tsx
+++ cbApp/App.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import  './scr/output.css';
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS!</h1>
+      </header>
+    </div>
   );
 };
 
