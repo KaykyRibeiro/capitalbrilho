4a5292b0091b63c2ba6c3d9d00d8738d7f8a115c
Deivid Santos <deividsantos1506@gmail.com>
2024-07-26 13:32:46 -0300

    Limpeza do Banco
    Limpa do SQL e do SMysql

--- cbApp/App.tsx
+++ cbApp/App.tsx
@@ -1,24 +1,10 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-//import { getDatabaseConnection } from './src/database/conexao';
 
 const App = () => {
-  useEffect(() => {
-    const checkDatabaseConnection = async () => {
-      try {
-        //const db = getDatabaseConnection();
-        console.log('Database opened successfully');
-      } catch (error) {
-        console.error('Database connection error:', error);
-      }
-    };
-
-    checkDatabaseConnection();
-  }, []);
-
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Check the console for database onnection status.</Text>
+      <Text>Hello World</Text>
     </View>
   );
 };
